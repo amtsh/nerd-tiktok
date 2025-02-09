0cfc03a7a269a3f9ea54937b8ecfc6918914577e
Amit Shinde <shinde13amit@gmail.com>
2025-02-09 03:34:49 +0100

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 // import { DarkModeToggle } from "@/components/DarkModeToggle";
 import { Suspense } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,6 +34,7 @@ export default function RootLayout({
               </div>
             </ThemeProvider>
           </Suspense>
+          <Analytics />
         </body>
       </html>
     </>
