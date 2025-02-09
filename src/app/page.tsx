3fa6aabc0331291531ddc6b744b317da3bdb3636
Amit Shinde <shinde13amit@gmail.com>
2025-02-09 02:21:06 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,43 +36,45 @@ const gradients = [
 
 export default function Home() {
   return (
-    <div className="min-h-screen py-16">
-      {/* start: apple books design */}
-      <div className="flex justify-center items-center min-h-screen">
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {topics.map((topic, index) => (
-            <Link
-              key={index}
-              href={`/feed?topic=${topic.toLowerCase().replace(/\s+/g, "-")}`}
-              legacyBehavior
-            >
-              <div className="flex flex-col items-center cursor-pointer">
-                <div
-                  className={`w-40 h-56 bg-gradient-to-b ${
-                    gradients[index % gradients.length]
-                  } rounded-md shadow-xs shadow-black relative`}
-                >
-                  <h2 className="flex justify-center items-center text-center text-white text-xl pt-8">
-                    {topic}
-                  </h2>
-                  <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50 rounded-md"></div>
-                </div>
-                {/* <p className="text-sm mt-2">Elbert Hubbard</p> */}
-                {/* <button className="mt-2 px-4 py-1 border rounded-full text-xs">
+    <>
+      <div className="min-h-screen py-16">
+        {/* start: apple books design */}
+        <div className="flex justify-center items-center min-h-screen">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {topics.map((topic, index) => (
+              <Link
+                key={index}
+                href={`/feed?topic=${topic.toLowerCase().replace(/\s+/g, "-")}`}
+                legacyBehavior
+              >
+                <div className="flex flex-col items-center cursor-pointer">
+                  <div
+                    className={`w-40 h-56 bg-gradient-to-b ${
+                      gradients[index % gradients.length]
+                    } rounded-md shadow shadow-black relative`}
+                  >
+                    <div className="flex justify-center items-center text-center text-white pt-8">
+                      {topic}
+                      {/* inner book border effect */}
+                      <div className="absolute inset-2 border border-white opacity-20"></div>
+                    </div>
+                    <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50 rounded-md"></div>
+                  </div>
+                  {/* <p className="text-sm mt-2">Elbert Hubbard</p> */}
+                  {/* <button className="mt-2 px-4 py-1 border rounded-full text-xs">
                   GET
                 </button> */}
-              </div>
-            </Link>
-          ))}
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
+        {/* end: apple books design */}
       </div>
-      {/* end: apple books design */}
 
-      <div className="fixed bottom-0 left-0 w-full flex justify-center pb-4">
-        <span className="text-xs text-gray-500">
-          <s>Doomscrolling</s> -&gt; Curiosity scrolling
-        </span>
+      <div className="w-full flex justify-center pb-4">
+        <span className="text-sm text-gray-400">Heal from doomscrolling</span>
       </div>
-    </div>
+    </>
   );
 }
