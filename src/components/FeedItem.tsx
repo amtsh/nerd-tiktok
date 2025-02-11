b155b4c8a7df89e27140c1c0ab8c0005c6534529
Amit Shinde <shinde13amit@gmail.com>
2025-02-11 02:08:05 +0100

    Update FeedItem.tsx

--- src/components/FeedItem.tsx
+++ src/components/FeedItem.tsx
@@ -15,11 +15,7 @@ export function FeedItem({ page }: FeedItemProps) {
   return (
     <div className="h-screen w-full flex items-center justify-center snap-start relative">
       <div className="h-full w-full relative">
-        <div className="absolute bottom-[15vh] left-0 right-0 p-6">
-          {/* <h2 className="text-2xl drop-shadow-lg mb-3 opacity-50">
-            {page.title}
-          </h2> */}
-
+        <div className="absolute bottom-[calc(100px+env(safe-area-inset-bottom))] left-0 right-0 p-4">
           <ul className="text-lg md:text-xl opacity-90">
             {page.extract.split(". ").map((sentence, index) => (
               <li className="mb-4" key={index}>
@@ -29,8 +25,8 @@ export function FeedItem({ page }: FeedItemProps) {
           </ul>
 
           {/* <!-- Bottom Section --> */}
-          <div className="mt-16">
-            <div className="flex items-center space-x-2">
+          <div className="mt-8">
+            <div className="flex justify-right items-center space-x-2">
               <div className="flex flex-col">
                 <span className="text-sm font-medium">{page.title}</span>
                 <span className="text-xs text-gray-400">
@@ -39,7 +35,7 @@ export function FeedItem({ page }: FeedItemProps) {
               </div>
             </div>
 
-            {/* <!-- Icons Section --> */}
+            {/* <!-- Icons Section --> 
             <div className="flex flex-col items-end space-y-8 text-gray-400 absolute bottom-6 right-4">
               <div className="flex items-center space-x-1">
                 <Heart size={24} />
@@ -51,6 +47,7 @@ export function FeedItem({ page }: FeedItemProps) {
                 <span className="text-xs">1</span>
               </div>
             </div>
+            */}
           </div>
         </div>
       </div>
