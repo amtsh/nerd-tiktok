a6fb1418df9cf369f136027964e85efabf50908d
Amit Shinde <shinde13amit@gmail.com>
2025-02-11 03:41:43 +0100

    Update route.ts

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -5,20 +5,29 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-export async function GET(request: Request) {
+var cachedTopics: string[] = [];
+
+export async function GET() {
   const NUM_TOPICS = 5;
   const prompt = `Generate a list of ${NUM_TOPICS} trending, interesting, simple, no-nonsense, concisely named topics that sparks curiosity. Only output comma separated list without any numbering.`;
 
   try {
+    if (cachedTopics.length > 0) {
+      return NextResponse.json(cachedTopics);
+    }
+
     const completion = await openai.chat.completions.create({
       messages: [{ role: "developer", content: prompt }],
       model: "gpt-3.5-turbo",
       store: false,
     });
 
-    const topics = completion.choices[0].message.content;
+    const content = completion.choices[0].message.content;
+    const topics = content?.split(", ") || [];
 
-    return NextResponse.json(topics?.split(", ") || []);
+    cachedTopics = topics;
+
+    return NextResponse.json(topics);
   } catch (error) {
     if (error instanceof Error) {
       console.error("Error fetching topics:", error.message);
