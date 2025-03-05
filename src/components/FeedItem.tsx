6716103c9e7fc64a6b5e6ff7a437c7ef398cdc20
Amit Shinde <shinde13amit@gmail.com>
2025-03-05 01:18:27 +0100

    margin

--- src/components/FeedItem.tsx
+++ src/components/FeedItem.tsx
@@ -33,7 +33,7 @@ export function FeedItem({ page, bookTitle }: FeedItemProps) {
                 <span className="text-sm text-gray-400 font-medium">
                   {page.pageNum}. {page?.pageTitle}
                 </span>
-                <span className="text-xs text-gray-400 capitalize ml-3">
+                <span className="text-xs text-gray-400 capitalize">
                   {bookTitle}
                 </span>
               </div>
