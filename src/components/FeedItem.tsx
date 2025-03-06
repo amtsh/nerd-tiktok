17a6d26af70e8451a3f65fc5eb7e6ff38080edf0
Amit Shinde <ashinde@spotify.com>
2025-03-06 14:02:22 +0100

    Update FeedItem.tsx

--- src/components/FeedItem.tsx
+++ src/components/FeedItem.tsx
@@ -15,10 +15,10 @@ export function FeedItem({ page, bookTitle }: FeedItemProps) {
   if (!page) return null;
 
   return (
-    <div className="h-screen w-full flex items-center justify-center snap-start relative">
-      <div className="h-full w-full relative">
+    <div className="h-screen w-full flex items-center justify-center snap-start relative text-white">
+      <div className="h-full w-full relative p-4">
         <div className="absolute bottom-[calc(100px+env(safe-area-inset-bottom))] left-0 right-0 p-4">
-          <ul className="text-xl md:text-2xl opacity-90">
+          <ul className="text-xl md:text-2xl opacity-90 leading-relaxed">
             {page.pageContent?.split(". ").map((sentence, index) => (
               <li className="mb-4" key={index}>
                 {sentence}.
@@ -26,9 +26,9 @@ export function FeedItem({ page, bookTitle }: FeedItemProps) {
             ))}
           </ul>
 
-          {/* <!-- Bottom Section --> */}
+          {/* Bottom Section */}
           <div className="mt-8">
-            <div className="flex justify-right items-center space-x-2">
+            <div className="flex justify-between items-center">
               <div className="flex flex-col">
                 <span className="text-sm text-gray-400 font-medium">
                   {page.pageNum}. {page?.pageTitle}
@@ -38,20 +38,6 @@ export function FeedItem({ page, bookTitle }: FeedItemProps) {
                 </span>
               </div>
             </div>
-
-            {/* <!-- Icons Section --> 
-            <div className="flex flex-col items-end space-y-8 text-gray-400 absolute bottom-6 right-4">
-              <div className="flex items-center space-x-1">
-                <Heart size={24} />
-                <span className="text-xs">1</span>
-              </div>
-
-              <div className="flex items-center space-x-1">
-                <MessageCircle size={24} />
-                <span className="text-xs">1</span>
-              </div>
-            </div>
-            */}
           </div>
         </div>
       </div>
