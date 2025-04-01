11646f5f4112aa61f51889b32b2f9a133645670f
Amit Shinde <ashinde@spotify.com>
2025-04-01 23:24:49 +0200

    add mock endpoint

---
+++ src/app/api/mock-api/mock-data.ts
@@ -0,0 +1,36 @@
+export const mockBook = {
+  id: "book-123",
+  topic: "The Wonders of Space",
+  pages: [
+    {
+      pageNum: 1,
+      pageTitle: "Introduction to Space",
+      pageContent:
+        "Space is vast and mysterious It contains stars, planets, and galaxies Space is vast and mysterious It contains stars, planets, and galaxies . Space is vast and mysterious. It contains stars, planets, and galaxies. Space is vast and mysterious. It contains stars, planets, and galaxies.",
+    },
+    {
+      pageNum: 2,
+      pageTitle: "The Solar System",
+      pageContent:
+        "Space is vast and mysterious It contains stars, planets, and galaxies Space is vast and mysterious It contains stars, planets, and galaxies . Space is vast and mysterious. It contains stars, planets, and galaxies. Space is vast and mysterious. It contains stars, planets, and galaxies.",
+    },
+    {
+      pageNum: 3,
+      pageTitle: "Black Holes",
+      pageContent:
+        "Space is vast and mysterious It contains stars, planets, and galaxies Space is vast and mysterious It contains stars, planets, and galaxies . Space is vast and mysterious. It contains stars, planets, and galaxies. Space is vast and mysterious. It contains stars, planets, and galaxies.",
+    },
+    {
+      pageNum: 4,
+      pageTitle: "The Milky Way",
+      pageContent:
+        "Space is vast and mysterious It contains stars, planets, and galaxies Space is vast and mysterious It contains stars, planets, and galaxies . Space is vast and mysterious. It contains stars, planets, and galaxies. Space is vast and mysterious. It contains stars, planets, and galaxies.",
+    },
+    {
+      pageNum: 5,
+      pageTitle: "Exploring Space",
+      pageContent:
+        "Space is vast and mysterious It contains stars, planets, and galaxies Space is vast and mysterious It contains stars, planets, and galaxies . Space is vast and mysterious. It contains stars, planets, and galaxies. Space is vast and mysterious. It contains stars, planets, and galaxies.",
+    },
+  ],
+};
