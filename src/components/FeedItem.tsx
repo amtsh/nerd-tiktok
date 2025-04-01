7c590cf43cf3563f78718cd3b45b5742ea0bc4c4
Amit Shinde <ashinde@spotify.com>
2025-04-01 23:50:19 +0200

    Update FeedItem.tsx

--- src/components/FeedItem.tsx
+++ src/components/FeedItem.tsx
@@ -15,29 +15,28 @@ export function FeedItem({ page, bookTitle }: FeedItemProps) {
   if (!page) return null;
 
   return (
-    <div className="h-screen w-full flex items-center justify-center snap-start relative text-white">
-      <div className="h-full w-full relative p-4">
-        <div className="absolute bottom-[calc(100px+env(safe-area-inset-bottom))] left-0 right-0 p-4">
-          <ul className="text-xl md:text-2xl opacity-90 leading-relaxed">
-            {page.pageContent?.split(". ").map((sentence, index) => (
-              <li className="mb-4" key={index}>
-                {sentence}.
-              </li>
-            ))}
-          </ul>
+    <div className="h-screen w-full flex flex-col justify-between snap-start relative text-white">
+      {/* Top Section */}
+      <div className="p-4 pt-[calc(100px+env(safe-area-inset-top))]">
+        <ul className="text-xl md:text-2xl opacity-90 leading-relaxed">
+          {page.pageContent?.split(". ").map((sentence, index) => (
+            <li className="mb-4" key={index}>
+              {sentence}.
+            </li>
+          ))}
+        </ul>
+      </div>
 
-          {/* Bottom Section */}
-          <div className="mt-8">
-            <div className="flex justify-between items-center">
-              <div className="flex flex-col">
-                <span className="text-sm text-gray-400 font-medium">
-                  {page.pageNum}. {page?.pageTitle}
-                </span>
-                <span className="text-xs text-gray-400 capitalize mt-2">
-                  {bookTitle}
-                </span>
-              </div>
-            </div>
+      {/* Bottom Section */}
+      <div className="p-4 pb-[calc(100px+env(safe-area-inset-bottom))]">
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col">
+            <span className="text-sm text-gray-300 font-medium">
+              {page.pageNum}. {page?.pageTitle}
+            </span>
+            <span className="text-xs text-gray-400 capitalize mt-2">
+              {bookTitle}
+            </span>
           </div>
         </div>
       </div>
